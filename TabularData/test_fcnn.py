69731521761b34388cc1a475a50be5eca28c45de
ale_9702 <alessandroruffolo29@gmail.com>
2025-05-02 16:10:36 +0200

    Adjust files

--- TabularData/test_fcnn.py
+++ TabularData/test_fcnn.py
@@ -3,7 +3,7 @@ import numpy as np
 from fcnn import FCNN
 
 # Load the dataset
-df = pd.read_excel('/kaggle/input/collision/collision.xlsx')
+df = pd.read_excel('/kaggle/input/collisionfcnn/collision.xlsx')
 
 # Separate features and target
 X = df.drop('collision', axis=1).values
