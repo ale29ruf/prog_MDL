a48c6bc82a68b195d6e788e0679f8e4cb1b9332e
ale_9702 <alessandroruffolo29@gmail.com>
2025-05-13 15:34:41 +0200

    Modifying fcnn execution

--- TabularData/collision/experiment_collision.py
+++ TabularData/collision/experiment_collision.py
@@ -190,7 +190,7 @@ percentages = [0.1,
                ]
 
 knn = [ "1", "2", "3", "4", "5"]
-class_accuracy = ["1.0", "0.99", "0.99", "0.99", "0.99"]
+class_accuracy = ["0.99", "0.98", "0.98", "0.98", "0.98"]
 
 metrics = ['time',
            'carbon',
